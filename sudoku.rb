7c9ea24b6f799cabca48f85bef91ead50229db5f
Simon Woolf <simon@simonwoolf.net>
2013-12-09 16:37:53 +0000

    now shows sudoku

--- sudoku.rb
+++ sudoku.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
+require_relative 'lib/grid'
+
+def random_sudoku
+  seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
+  sudoku = Grid.new(seed.join)
+  #sudoku.solve
+  sudoku.to_s.chars
+end
 
 get '/' do
-  @name = params[:name]
-  erb :index, {:layout => :layout}
+  @current_solution = random_sudoku
+  erb :index
 end
