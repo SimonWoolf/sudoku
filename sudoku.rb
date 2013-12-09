a2acb8dcd3990aa4b47a5764804c24c0b19081a6
Simon Woolf <simon@simonwoolf.net>
2013-12-09 17:36:22 +0000

    /solution page

--- sudoku.rb
+++ sudoku.rb
@@ -1,14 +1,25 @@
 require 'sinatra'
 require_relative 'lib/grid'
 
+enable :sessions
+
 def random_sudoku
   seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
   sudoku = Grid.new(seed.join)
   #sudoku.solve
-  sudoku.to_s.chars
 end
 
 get '/' do
-  @current_solution = random_sudoku
+  puzzle = random_sudoku.to_s
+  session[:puzzle] = puzzle
+  @current_puzzle = puzzle.gsub('0',' ')
+  erb :index
+end
+
+get '/solution' do
+  puzzle = session[:puzzle]
+  sudoku = Grid.new(puzzle)
+  sudoku.solve
+  @current_puzzle = sudoku.to_s
   erb :index
 end
