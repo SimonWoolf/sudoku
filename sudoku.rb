ac7bb2cbce2d6ef09ccf6734e187eead3e650129
Simon Woolf <simon@simonwoolf.net>
2013-12-10 16:56:37 +0000

    added rspec rack test

--- sudoku.rb
+++ sudoku.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require_relative 'lib/grid'
 
-enable :sessions
+enable :sessions  unless test?
 
 def random_sudoku
   seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
@@ -9,11 +9,35 @@ def random_sudoku
   #sudoku.solve
 end
 
-get '/' do
+def box_order_to_row_order(cells)
+  boxes = cells.each_slice(9).to_a
+  (0..8).to_a.inject([]) do |memo, i|
+    memo += boxes[i/3*3, 3].map do |box| 
+      box[(i % 3) * 3, 3]
+    end.flatten
+  end
+end
+
+def generate_new_puzzle
   puzzle = random_sudoku.to_s
   session[:puzzle] = puzzle
-  @current_puzzle = puzzle.gsub('0',' ')
+  puzzle.gsub('0',' ')
+end
+
+get '/' do
+  @check_solution = session[:check_solution]
+  session[:check_solution] = nil
+  
+  @current_puzzle = (session[:proposed_solution] || generate_new_puzzle).gsub('0',' ') 
+
   erb :index
+end
+
+post '/' do
+  cells = box_order_to_row_order(params['cell'])
+  session[:proposed_solution] = cells.map(&:to_i).join
+  session[:check_solution] = true
+  redirect to('/')
 end
 
 get '/solution' do
